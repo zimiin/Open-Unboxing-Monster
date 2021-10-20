290afe11f170fb0a4388ce8f6ca5682305527883
zimiin <kahin6105@gmail.com>
2021-10-20 17:48:04 +0900

    Add input

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import IndexTemplate from '../templates/IndexTemplate'
-import { VStack, Image, Flex } from '@chakra-ui/react'
-import { Spacer } from '@chakra-ui/layout'
-import logo from '../../assets/logo_h.svg'
-
 
 function IndexPage() {
   return (
-    <VStack bgColor='blue'>
-    {/* <Flex> */}
-      <Flex flex={1}>
-        <Image src={logo} htmlWidth='400' />
-      </Flex>
-      <Spacer />
-      <Image src={logo} htmlWidth='400' />
-    {/* </Flex> */}
-    </VStack>
+    <IndexTemplate />
   )
 }
 
--- src/components/templates/IndexTemplate.tsx
+++ src/components/templates/IndexTemplate.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
-import { VStack, Image, Flex } from '@chakra-ui/react'
-import { Spacer } from '@chakra-ui/layout'
-import './IndexTemplate.css'
+import { VStack, Image, Flex, Input } from '@chakra-ui/react'
+import { InputGroup, InputRightElement } from '@chakra-ui/input'
+import { Button, IconButton } from '@chakra-ui/button'
 import logo from '../../assets/logo_h.svg'
 
 function IndexTemplate() {
   return (
-    <Flex bgColor='blue' h='100vh' flexDir='column'>
-    {/* VStack h='100%' bgColor='blue' > */}
-      <Spacer />
-      <Image src={logo} flex={1} htmlWidth='400'/>
-      <Image src={logo} htmlWidth='400' />
-    {/* </VStack> */}
-    </Flex>
+    <VStack
+      h='100vh'
+      justifyContent='center'
+      bgColor='white'
+    >
+      <Image
+        src={logo}
+        htmlWidth='370'
+      />
+      
+      <InputGroup>
+        <Input
+          placeholder='닉네임을 입력하세요'
+          _placeholder={{ color: 'gray.900' }}
+          bg='blue.100'
+          borderColor='blue.300'
+          _hover={{ borderColor: 'blue.300', background: 'white' }}
+          focusBorderColor='blue.300'
+          variant='filled'
+          textColor='gray.900'
+          w='450px'
+          borderRadius='18px'
+        />
+
+        <InputRightElement>
+          {/* <IconButton aria-label='Search' icon={} */}
+        </InputRightElement>
+      </InputGroup>
+    </VStack>
   )
 }
 
